94e8f4a233d9c723b7fd48ff4c9add9b7c066960
MikalaiKholin <48877386+MikalaiKholin@users.noreply.github.com>
2021-05-30 23:50:09 +0300

    Update StringParser.kt

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -6,19 +6,21 @@ class StringParser {
     fun getResult(inputString: String): Array<String> {
         //throw NotImplementedError("Not implemented")
         var k : Array<String> = arrayOf()
+        var str: String = inputString
+
 
         k=getStr(inputString)
 
-        for (e in k){
-            println(e)
-        }
+
 
         return (k)
     }
 
     fun getSubStr (inputString: String): String{
-
-        return("df")
+        val pattern = "[<].*?[>]|[(].*?[)]|[\\[].*?[\\]]".toRegex()
+        var str = pattern.find(inputString)
+        println(str?.value)
+        return(str?.value.toString())
     }
 
     fun getStr (inputString: String): Array<String>{
@@ -27,15 +29,15 @@ class StringParser {
         val pattern = "[<].*?[>]|[(].*?[)]|[\\[].*?[\\]]".toRegex()
         val str = pattern.findAll(inputString)
         for (s in str){
-            var tmp : MutableList<String> = getStr(s.value.drop(1).dropLast(1)).toMutableList()
-            if (tmp.size !=0) {
-                for (t in tmp){
-                    res.add(t)
-                }
-             //println (getStr(s.value.drop(1).dropLast(1)).elementAt(0))
+            var tmp: String = s.value.drop(1).dropLast(1).toString()
+
+            res.add(tmp)
+            println (tmp)
+            for (s in getStr(tmp)){
+                var tmp: String = s
+                res.add(tmp)
             }
-            res.add(s.value.drop(1).dropLast(1))
-            println (s.value.drop(1).dropLast(1))
+
         }
         return (res.toTypedArray())
     }
